2e87bf467401bd00ad45e042e77b71cb0bb64846
teamatsouthwestcare <61629812+teamatsouthwestcare@users.noreply.github.com>
2020-03-03 20:19:04 +0000

    Res

---
+++ access.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'vf36937k';
+$imSettings['access']['users'] = array(
+	'info@example.com' => array(
+		'groups' => array('hx2euey2'),
+		'id' => '49bjgt27',
+		'firstname' => 'Admin',
+		'lastname' => '',
+		'password' => '12345',
+		'email' => 'info@example.com',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('49bjgt27');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
---
+++ search.inc.php
@@ -0,0 +1,44 @@
+<?php
+
+/*****************
+	GENERAL SETTINGS
+*****************/
+$imSettings['search']['general'] = array(
+	'menu_position' => 'left',
+	'defaultScope' => array(
+		'0' => 'index.php',
+		'4' => 'vacancies.html',
+		'5' => 'jobs.html',
+		'6' => 'contact.html',
+		'7' => 'about.html',
+		'8' => 'partners_contact.html',
+		'10' => 'regional-care-manager-25-02-20-1.html',
+		'11' => 'carer-25-02-20.html',
+		'12' => 'senior-team-leader-25-02-20.html'
+	),
+	'extendedScope' => array(
+	)
+);
+
+/*****************
+	PRODUCTS SEARCH
+*****************/
+$imSettings['search']['products'] = array(
+);
+
+/*****************
+	IMAGES SEARCH
+*****************/
+$imSettings['search']['images'] = array(
+);
+
+/*****************
+	VIDEOS SEARCH
+*****************/
+$imSettings['search']['videos'] = array(
+);
+$imSettings['search']['dynamicobjects'] = array(
+
+);
+
+// End of file search.inc.php
